db2276ffc79d383ed5cbfc99e4d4a8e70547d4ad
scx1332 <sieciech@gmail.com>
2026-03-10 20:32:40 +0100

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-set -x
+  set -euo pipefail
+  set -x
 
-POSTGRES_DATA_DIR="./postgres_data"
-REDIS_DATA_DIR="./redis_data"
+  POSTGRES_DATA_DIR="./postgres_data"
+  REDIS_DATA_DIR="./redis_data"
 
-ensure_data_dir() {
-  local dir="$1"
-  local image="$2"
-  local uid="$3"
-  local gid="$4"
+  ensure_data_dir() {
+    local dir="$1"
+    local uid="$2"
+    local gid="$3"
 
-  if [[ -d "${dir}" ]]; then
-    return
-  fi
+    if [[ -d "${dir}" ]]; then
+      return
+    fi
 
-  mkdir -p "${dir}"
-  docker run --rm -v "${dir}:/data" --user root "${image}" sh -c "chown -R ${uid}:${gid} /data"
-}
+    mkdir -p "${dir}"
 
-export BUILD_VERSION="${BUILD_VERSION:-$(git rev-parse HEAD)}"
+    if chown -R "${uid}:${gid}" "${dir}" 2>/dev/null; then
+      :
+    else
+      sudo chown -R "${uid}:${gid}" "${dir}"
+    fi
+  }
 
-ensure_data_dir "${POSTGRES_DATA_DIR}" "postgres:17-alpine" "70" "70"
-ensure_data_dir "${REDIS_DATA_DIR}" "redis:8-alpine" "999" "999"
+  export BUILD_VERSION="${BUILD_VERSION:-$(git rev-parse HEAD)}"
 
-docker compose down
-docker compose up -d --build
+  ensure_data_dir "${POSTGRES_DATA_DIR}" "70" "70"
+  ensure_data_dir "${REDIS_DATA_DIR}" "999" "999"
+
+  docker compose down
+  docker compose up -d --build
